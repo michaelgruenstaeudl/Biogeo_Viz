2b3a3409f0165c206d890301ad9f1086d4e1e8fa
Michael Gruenstaeudl <mi.gruenstaeudl@gmail.com>
2015-11-17 19:43:08 +0100

    2015.11.17

--- InPrep_Figure4.R
+++ InPrep_Figure4.R
@@ -3,49 +3,38 @@
 #copyright = "Copyright (C) 2014-2015 Michael Gruenstaeudl"
 #version = "2015.11.17.1600"
 
-load("/home/michael/research/manuscripts/02_Tolpis_biogeo/03_Figures/NEW_Figure_6/Figure6_data.rda") 
-
 library(ggplot2)
 library(grid)
 
-node1_data = data[which(data[,1]=="one"),]
-node1_data = node1_data[which(node1_data[,6]=="no"),]
-node1_data = node1_data[which(node1_data[,3] %in% c("A","C","E","M")),]
-# Adjust order of levels
-node1_data[,3] = factor(node1_data[,3], levels = c("A", "C", "E", "M"))
-node1_data[,7] = rep("node1", nrow(node1_data))
-colnames(node1_data)[7] = "node_number"
+inFn = "/home/michael_science/research/manuscripts/02_Tolpis_biogeo/03_Figures/2015_FU-Berlin/Figure_4/data/Figure4_data.csv"
+data = read.csv(inFn)
+clade_data = data[which(data[,2] %in% c("AZ","CI","CM","MA")),]
+clade_data[,2] = factor(clade_data[,2], levels = c("AZ", "CI", "CM", "MA"))  # Adjust order of level
 
-# What is nowadays node 2 has previously been node 3
-node2_data = data[which(data[,1]=="three"),]
-node2_data = node2_data[which(node2_data[,6]=="no"),]
-node2_data = node2_data[which(node2_data[,3] %in% c("A","C","E","M")),]
-# Adjust order of levels
-node2_data[,3] = factor(node2_data[,3], levels = c("A", "C", "E", "M"))
-node2_data[,7] = rep("node2", nrow(node2_data))
-colnames(node2_data)[7] = "node_number"
+printerfriendly_bw_palette = c("#ffffff", "#cccccc", "#969696", "#000000")
 
-node_data = rbind(node1_data, node2_data)
-
-plot_data = ggplot(data=node_data, aes(x=treetype, y=pc)) +
-    geom_bar(stat='identity', aes(alpha=0.5)) +
-    coord_flip() +
-    facet_grid(node_number ~ area) +
-    scale_x_discrete(limits=c("stAll3", "stTwoLC", "gtETS", "gtB12", "gtA19"), labels=c("All3Nucl", "2LCNucl", "ETS", "B12", "A19")) +
+plot_data = ggplot(data=clade_data, aes(x=treetype, y=pc, fill=area)) +
+    geom_bar(stat="identity", colour="black") +
+    coord_flip() + # x- and y-axes have been switched [coord_flip() !]
+    facet_grid(clade ~ exclusion) +
+    scale_x_discrete(limits=c("stLS2", "stLS1", "gtETS", "gtB12", "gtA19"), labels=c("LS2", "LS1", "ETS", "B12", "A19")) +
     scale_y_continuous(breaks=c(0.00, 0.25, 0.50, 0.75, 1.00), labels=c("","0.25","","0.75","")) +
     theme_bw() +
-    # x- and y-axieshave been switched [coord_flip() !]
-    theme(panel.grid.major.x = element_line(size = 0.8),
+    scale_fill_manual(values = printerfriendly_bw_palette,
+                      name="Ancestral area",
+                      breaks=c("AZ", "CI", "CM", "MA"),
+                      labels=c("Azores", "Canary Islands", "Continental Mediterranean", "Madeira")) +
+    theme(text = element_text(size=28),
+          panel.grid.major.x = element_line(size = 0.8),
           panel.grid.major.y = element_blank(),
           panel.grid.minor.y = element_blank(),          
           strip.background=element_rect(fill="white"),
-          panel.margin = unit(0.75, "lines")) +
-    ggtitle("Ancestral Area Reconstructions under BI\n") +
-    xlab("Loci (or combinations) under study\n") + 
+          panel.margin = unit(0.75, "lines"),
+          plot.title = element_text(size=28)) +
+    ggtitle("Archipelagoes\n") +
+    xlab("Locus or locus sets\n") + 
     ylab("\nPercent of posterior tree distribution")
-    # Remove the legend to the right
-    theme(legend.position = "none")
 
-svg("/home/michael/Desktop/TolpisBiogeo_Figure4_bothNodes.svg", width=8, height=6)
+svg("/home/michael/Desktop/TolpisBiogeo_Figure4_bothNodes.svg", width=8, height=4)
 plot_data
 dev.off()
